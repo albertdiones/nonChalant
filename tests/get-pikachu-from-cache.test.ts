57914feefe5643cf56d360951d88ea71b124fe37
Albert Diones <adiones@addteam.org>
2024-09-11 22:52:13 +0800

    cached get pikachu data test

---
+++ tests/get-pikachu-from-cache.test.ts
@@ -0,0 +1,62 @@
+import { test, expect } from '@jest/globals'
+import HttpClient from '../index';
+
+test(
+    'get pikachu data',
+    async () => {
+        const client = new HttpClient(
+            {
+                cache: new FakeCache(),
+                logger: console
+            }
+        );
+
+        const {response, fromCache} = await client.getWithCache(
+            'https://pokeapi.co/api/v2/pokemon/pikachu'
+        );
+
+        expect(fromCache).toBe(true);
+
+        expect(
+            response
+        ).not.toBeFalsy();
+
+        expect(
+            response.types
+        ).not.toBeFalsy();
+
+        expect(
+            response.types.length
+        ).toBeGreaterThanOrEqual(1);
+
+        expect(
+            response.types[0].type.name
+        ).toBe('water');
+    }
+)
+
+class FakeCache {
+    async getItem(key: string): Promise<string | null> {
+        return Promise.resolve(cachedResponse);
+    }
+  
+    setItem(
+        key: string, 
+        value: string,
+        expirationSeconds: number
+    ): void { 
+    }
+}
+
+const cachedResponse = `{
+    "types": [
+      {
+        "slot": 1,
+        "type": {
+          "name": "water",
+          "url": "https://pokeapi.co/api/v2/type/13/"
+        }
+      }
+    ],
+    "weight": 60
+}`
