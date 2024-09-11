eb2a9da770a0e200a083ceb9d8a8a77ebe2272ba
Albert Diones <adiones@addteam.org>
2024-09-11 23:29:10 +0800

    delay test with minor issue: first call already has delay

---
+++ tests/get-pikachu-multiple-times-with-timeout.test.ts
@@ -0,0 +1,61 @@
+import { test, expect } from '@jest/globals'
+import HttpClient from '../index';
+
+export class CacheViaNothing {
+    async getItem(key: string): Promise<string | null> {
+        return null;
+    }
+  
+    setItem(
+        key: string, 
+        value: string,
+        expirationSeconds: number
+    ): void { 
+    }
+}
+
+test(
+    'get pikachu data',
+    async () => {
+        const client = new HttpClient(
+            {
+                cache: new CacheViaNothing(),
+                minTimeoutPerRequest: 400,
+                logger: console
+            }
+        );
+        const startTime = Date.now();
+
+        // right away
+        client.getWithCache(
+            'https://pokeapi.co/api/v2/pokemon/pikachu'
+        );
+        
+        // after 400 ms...
+        client.getWithCache(
+            'https://pokeapi.co/api/v2/pokemon/bulbasaur'
+        );
+
+        // after 800ms
+        client.getWithCache(
+            'https://pokeapi.co/api/v2/pokemon/charmander'
+        );
+
+        // after 1200ms
+        client.getWithCache(
+            'https://pokeapi.co/api/v2/pokemon/squirtle'
+        );
+
+        // after 1600ms
+        await client.getWithCache(
+            'https://pokeapi.co/api/v2/pokemon/nidorino'
+        );
+
+        const timeElapsed = Date.now() - startTime;
+
+        expect(
+            timeElapsed
+        ).toBeGreaterThanOrEqual(1600);
+
+    }
+)
