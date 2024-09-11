52ba58e77c8b6bcf9a948e8a024d3da3532508ae
Albert Diones <adiones@addteam.org>
2024-09-11 22:25:59 +0800

    stash: commit

---
+++ tests/get-pikachu.test.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@jest/globals'
+import HttpClient from '../index';
+
+test(
+    'get pikachu data',
+    () => {
+        const client = new HttpClient(
+            
+        )
+    }
+)
