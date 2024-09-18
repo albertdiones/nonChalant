da2e4b41a7d548ce84c254877a899607fc126a2f
Albert Diones <adiones@addteam.org>
2024-09-18 22:23:51 +0800

    stash

--- scheduleManager.ts
+++ scheduleManager.ts
@@ -26,6 +26,8 @@ export class PaddedScheduleManager implements AsyncTaskManagerInterface {
 
   minTimeoutPerRequest: number;
   maxRandomPreRequestTimeout: number;
+
+  lastFe
   
   constructor(
     minTimeoutPerRequest: number,
@@ -35,6 +37,31 @@ export class PaddedScheduleManager implements AsyncTaskManagerInterface {
     this.maxRandomPreRequestTimeout = maxRandomPreRequestTimeout;
   }
   add(task: () => Promise<any>): Promise<any> {
-    throw new Error("Method not implemented.");
+
+      // first fetch = no delay
+      let delayBeforeFetch = 0;   
+      if (this.lastFetchSchedule) {
+        // existing queue = calculate the delay
+        delayBeforeFetch = 
+          (this.lastFetchSchedule - Date.now())
+          + this.minTimeoutPerRequest 
+          + randomDelay;
+      }     
+
+      this.lastFetchSchedule = Date.now() + delayBeforeFetch;
+
+      this.logger?.info(`Fetching ${url} (delay: ${delayBeforeFetch})`);
+
+      return (
+        delayBeforeFetch <= 0 // negative delay will also be done instantly
+        ? this._fetch(url, fetchOptions) 
+        : Bun.sleep(delayBeforeFetch).then(
+            () => this._fetch(url, fetchOptions)
+        )
+      ).catch(
+        (e: Error) => {
+          this.logger?.warn(`Error occurred trying to access ${url} : ${e}`)
+        }
+      )
   }
 }
