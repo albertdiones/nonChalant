9f9532a5ef9359d50f5aad11dd9d92de6002df9f
Albert Diones <adiones@addteam.org>
2024-09-18 22:24:00 +0800

    Revert "stash"
    This reverts commit da2e4b41a7d548ce84c254877a899607fc126a2f.

--- scheduleManager.ts
+++ scheduleManager.ts
@@ -26,8 +26,6 @@ export class PaddedScheduleManager implements AsyncTaskManagerInterface {
 
   minTimeoutPerRequest: number;
   maxRandomPreRequestTimeout: number;
-
-  lastFe
   
   constructor(
     minTimeoutPerRequest: number,
@@ -37,31 +35,6 @@ export class PaddedScheduleManager implements AsyncTaskManagerInterface {
     this.maxRandomPreRequestTimeout = maxRandomPreRequestTimeout;
   }
   add(task: () => Promise<any>): Promise<any> {
-
-      // first fetch = no delay
-      let delayBeforeFetch = 0;   
-      if (this.lastFetchSchedule) {
-        // existing queue = calculate the delay
-        delayBeforeFetch = 
-          (this.lastFetchSchedule - Date.now())
-          + this.minTimeoutPerRequest 
-          + randomDelay;
-      }     
-
-      this.lastFetchSchedule = Date.now() + delayBeforeFetch;
-
-      this.logger?.info(`Fetching ${url} (delay: ${delayBeforeFetch})`);
-
-      return (
-        delayBeforeFetch <= 0 // negative delay will also be done instantly
-        ? this._fetch(url, fetchOptions) 
-        : Bun.sleep(delayBeforeFetch).then(
-            () => this._fetch(url, fetchOptions)
-        )
-      ).catch(
-        (e: Error) => {
-          this.logger?.warn(`Error occurred trying to access ${url} : ${e}`)
-        }
-      )
+    throw new Error("Method not implemented.");
   }
 }
